353aacbbc1b2206a5676b9c27dc17c80b00c92a6
Oliver Pruett <ospruett19@gmail.com>
2026-01-26 13:25:30 -0500

    updated README, added credential validator for db

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # budget-app
-To run use ```uvicorn main.src.service.budget_app:app --reload```
+To run use ```uvicorn main.src.service.budget_app:app --reload --log-level debug```
 
 Ensure that mongoDB is up an running as well
--- main/db/account_db.py
+++ main/db/account_db.py
@@ -8,16 +8,26 @@ class AccountDB:
         self.logger.info("AccountDB initialized.")
 
     def insert(self, account_data: str):
+        self.logger.info("Inserting new account...")
         self.collection.insert_one(account_data)
         self.logger.info("Insertion complete.")
 
     def find_by_field(self, field: str, val: str):
         entry = self.collection.find_one({field: val})
         del entry['_id']
+        del entry['password']
         return entry
+
+    def validate_credentials(self, username: str, password: str) -> dict | None:
+        account = self.collection.find_one({"user": username})
+        if account and account['password'] == password:
+            del account['_id']
+            del account['password']
+            return account
+        return None
     
     def update_account(self, id: str, account_data: str):
-        self.collection.update_one({"test_id": id}, {"$set": {"data": account_data}})
+        self.collection.update_one({"user_id": id}, {"$set": {"data": account_data}})
         return "Account updated successfully"
     
     def close(self):
