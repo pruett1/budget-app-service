89266bc609b180b6989e83ce3f306f6424d2aa5d
Oliver Pruett <ospruett19@gmail.com>
2026-02-03 23:01:36 -0500

    Started building out plaid client

--- main/src/helpers/plaid.py
+++ main/src/helpers/plaid.py
@@ -1,4 +1,5 @@
-import requests
+import httpx
+import json
 from main.env.envs import Env
 from logging import Logger
 
@@ -18,5 +19,58 @@ class Plaid:
         else:
             raise ValueError("Invalid env specified")
         
-    def create_link_token(self, user_id: str) -> str:
-        pass
+        self.client = httpx.Client(base_url=self.base_url, timeout=10.0)
+        
+    async def create_link_token(self, user_id: str) -> str:
+        path = "/link/token/create"
+        payload = {
+            "client_id": self.client_id,
+            "secret": self.secret,
+            "client_name": "pruett1-budget-app",
+            "user": {
+                "client_user_id": user_id
+            },
+            "products": ["transactions", "recurring_transactions", "investments", "liabilities"],
+            "country_codes": ["US"],
+            "language": "en"
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        try:
+            response = await self.client.post(path, json=payload, headers=headers)
+            response.raise_for_status()
+            data = response.json()
+            self.logger.info(f"Successfully created link token expiring at {data['expiration']}")
+            return data['link_token']
+        except httpx.HTTPStatusError as e:
+            self.logger.error(f"HTTP error while creating link token: {e.response.text}")
+            raise
+        except httpx.RequestError as e:
+            self.logger.error(f"Request error while creating link token: {str(e)}")
+            raise
+
+    async def exchange_public_token(self, public_token: str):
+        path = "/item/public_token/exchange"
+        payload = {
+            "client_id": self.client_id,
+            "secret": self.secret,
+            "public_token": public_token
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        try:
+            response = await self.client.post(path, json=payload, headers=headers)
+            response.raise_for_status()
+            data = response.json()
+            self.logger.info("Successfully exchanged public token for access token and item id")
+            return data['access_token'], data['item_id']
+        except httpx.HTTPStatusError as e:
+            self.logger.error(f"HTTP error while exchanging public token: {e.response.text}")
+            raise
+        except httpx.RequestError as e:
+            self.logger.error(f"Request error while exchanging public token: {str(e)}")
+            raise
+
+    
